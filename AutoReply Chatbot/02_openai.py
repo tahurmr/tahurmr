4eee40612d79a4664dae41155e310e52a70cc18f
Tahur Ramdul <114643433+tahurmr@users.noreply.github.com>
2024-10-22 21:26:17 +0530

    Update 02_openai.py

--- AutoReply Chatbot/02_openai.py
+++ AutoReply Chatbot/02_openai.py
@@ -7,28 +7,39 @@ client = OpenAI(
 )
 
 command = '''
-[20:30, 12/6/2024] Naruto: jo sunke coding ho sake?
-[20:30, 12/6/2024] Rohan Das: https://www.youtube.com/watch?v=DzmG-4-OASQ
-[20:30, 12/6/2024] Rohan Das: ye
-[20:30, 12/6/2024] Rohan Das: https://www.youtube.com/watch?v=DzmG-4-OASQ
-[20:31, 12/6/2024] Naruto: This is hindi
-[20:31, 12/6/2024] Naruto: send me some english songs
-[20:31, 12/6/2024] Naruto: but wait
-[20:31, 12/6/2024] Naruto: this song is amazing
-[20:31, 12/6/2024] Naruto: so I will stick to it
-[20:31, 12/6/2024] Naruto: send me some english song also
-[20:31, 12/6/2024] Rohan Das: hold on
-[20:31, 12/6/2024] Naruto: I know what you are about to send
-[20:32, 12/6/2024] Naruto: 😂😂
-[20:32, 12/6/2024] Rohan Das: https://www.youtube.com/watch?v=ar-3chBG4NU
-ye hindi English mix hai but best hai
-[20:33, 12/6/2024] Naruto: okok
-[20:33, 12/6/2024] Rohan Das: Haan
+[9:24 pm, 04/09/2024] طحور۔۔: Ok okk
+[9:25 pm, 04/09/2024] Parthiv Koli (CSMU): https://www.codsoft.in/
+[9:25 pm, 04/09/2024] Parthiv Koli (CSMU): Maja barobar Prathamesh pan internship karat aahe
+[9:25 pm, 04/09/2024] Parthiv Koli (CSMU): Me Ai ghetla, Tyne Python ghetla
+[10:05 pm, 04/09/2024] طحور۔۔: Achaa okk
+[12:46 pm, 06/10/2024] طحور۔۔: Hechi link patv na bhaii
+[12:54 pm, 06/10/2024] Parthiv Koli (CSMU): https://prodigyinfotech.dev/
+[8:08 pm, 06/10/2024] Parthiv Koli (CSMU): https://internship.aicte-india.org/
+[8:09 pm, 06/10/2024] Parthiv Koli (CSMU): Me ya var atta internship apply kele aahe
+[8:21 pm, 06/10/2024] طحور۔۔: Achaa
+[8:23 pm, 06/10/2024] Parthiv Koli (CSMU): Shortlist jhala var mail yeto
+[8:23 pm, 06/10/2024] Parthiv Koli (CSMU): Pan ya madhe Kai Kai Internship la Interview pan lagto
+[8:24 pm, 06/10/2024] Parthiv Koli (CSMU): Kai Kai government wale madhe Stipend pan aahe
+[8:25 pm, 06/10/2024] طحور۔۔: Ohh achhaa
+[8:25 pm, 06/10/2024] طحور۔۔: Tu shortlist zala ahe naa
+[8:26 pm, 06/10/2024] Parthiv Koli (CSMU): 3 madhe shortlist jhalo, 2 madhun call aale hote
+[8:26 pm, 06/10/2024] طحور۔۔: Interview zala kay
+[8:26 pm, 06/10/2024] Parthiv Koli (CSMU): Nahi, te nantar ghenar bole
+[8:26 pm, 06/10/2024] طحور۔۔: Achaa okk
+[8:26 pm, 06/10/2024] Parthiv Koli (CSMU): Mag me dusre apply kele
+[8:27 pm, 06/10/2024] طحور۔۔: Ok okk
+[8:27 pm, 06/10/2024] Parthiv Koli (CSMU): 3rd madhe shortlist jhalo aahe, tya madhe interview nahi aahe, tar te karel
+[8:27 pm, 06/10/2024] طحور۔۔: All domains wal??
+[8:28 pm, 06/10/2024] Parthiv Koli (CSMU): 1st wala aahe na, tya madhe Data Analyst cha aahe
+[8:28 pm, 06/10/2024] طحور۔۔: Acha
+[8:29 pm, 06/10/2024] طحور۔۔: Hechya mde ahe kay te
+[8:29 pm, 06/10/2024] Parthiv Koli (CSMU): Ho
+[8:29 pm, 06/10/2024] طحور۔۔: Ok okk
 '''
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a person named harry who speaks hindi as well as english. He is from India and is a coder. You analyze chat history and respond like Harry"},
+    {"role": "system", "content": "You are a person named Tahur who speaks hindi, marathi as well as english. He is from India and is a coder. You analyze chat history and respond like Tahur"},
     {"role": "user", "content": command}
   ]
 )
